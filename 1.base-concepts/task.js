22c766179a0e6a6d4ffde69e02f3ffbd872dc820
Alexander Tagiltsev <alextagiltsev@yandex.ru>
2023-11-01 09:53:11 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (!d) {
+    arr.push(-b / (2 * a));
+  }
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let formatedPercent = percent / 100 / 12;
+  if (+formatedPercent) {
+    let banking = amount - contribution;
+    let pay = banking * (formatedPercent + formatedPercent / (((1 + formatedPercent) ** countMonths) - 1));
+    let total = +(pay * countMonths).toFixed(2);
+    return total;
+  }
 }
