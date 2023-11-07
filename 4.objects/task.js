b74f7e9facd378e6e6815537bca7d4e120c59cb8
Alexander Tagiltsev <alextagiltsev@yandex.ru>
2023-11-07 09:31:24 +0300

    obj homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+
+    marks && marks.length && this.marks.push(marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks.length) {
+        let sum = 0;
+        let count = 0;
+        for (let i = 0; i < this.marks.length; i++) {
+            count += 1;
+            sum += this.marks[i];
+        }
+        return sum / count;
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
